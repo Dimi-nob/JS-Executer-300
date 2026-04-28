c36646d41ee16e5b60048f3c94431e6eb89972d5
Dimi-nob <dimitrijea1513@gmail.com>
2026-04-28 19:15:02 +0200

    Update script.js

--- js executer 3000/script.js
+++ js executer 3000/script.js
@@ -1,42 +1,4 @@
 addEventListener("DOMContentLoaded", () => {
     
-let meinName = "Dimi"
 
-
-let meinAlter = 15
-let meinHobby = "DnD und flohmärkte" 
-
-console.log(meinAlter + meinHobby)
-
-
-meinAlter = 16
-console.log(meinAlter)
-
-let vorstellung = meinName
-console.log(vorstellung)	
-
-let geburtsJahr = 2010	
-meinAlter = 2026 - geburtsJahr
-console.log(meinAlter)
-
-console.log("Ich wurde im Jahr " + geburtsJahr + " geboren und mein Hobby ist " + meinHobby)
-
-
-
-let benutzerName = prompt("Wie heißt du?") 
-
-console.log("Wilkommen " + benutzerName)
-
-
-
-let note = 5
-
-if(note < 3)
-{
-    console.log("Deine Note(" + note + ") ist besser als drei")
-}
-else
-{
-    console.log("Deine Note ist drei oder schlechter google.com/search?q=nachhilfe")
-}
 });
